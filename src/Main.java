55bd3dfa9ed39f8e87899ef6f3e0dde21f35f35a
JSADeveloper <josh.stigall@justsolutionsag.ca>
2018-02-23 11:33:24 -0600

    Commit from CodingProjects. Should go to the TestSalesforce repo in Git.

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    //THIS IS JUST A TEST OF A DIFFERENT REPO IN A DIFFERENT FOLDER.
+}
